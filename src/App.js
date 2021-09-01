030b97707856824b97ab5f0e3d997ff7db4070f7
zortol <nepal_14221@hotmail.com>
2021-09-01 12:18:39 +0100

    Added remote origin

--- src/App.js
+++ src/App.js
@@ -44,8 +44,8 @@ function App() {
     console.log('Date in ' + timezone + ': ' + date);
     return date
   }
-    
-    const getTimeZoneName = (timezone) => {
+
+  const getTimeZoneName = (timezone) => {
     let date = now.toLocaleString("en-GB", { timeZoneName: "long", timeZone: timezone });
     return date
   }
@@ -57,50 +57,49 @@ function App() {
 
   }, [0])
 
-  return (
+  return (<>
+
     <div className="container">
-              <div className="row my-5 text-center">
-            <div className="col-auto mx-auto">
-              <h4>London</h4>
-              <h5>{getTimeZone("Europe/London")}</h5>
-            </div>
-            <div className="col-auto mx-auto">
-              <h4>Singapore</h4>
-              <h5>{getTimeZone("Asia/Singapore")}</h5>
-            </div>
-          </div>
-                </div>
-      
-    <div className="Container">
-      <div className="col-6 mx-auto my-5">
-
-
-
-        {/* <h6>Current Time in UK is: {CurrentUKTime.toLocaleTimeString()}</h6>
-        <h6>Current Time in SG is: {CurrentSGTime.toLocaleTimeString()}</h6> */}
-        <form className="row my-3 text-center">
-          <div className="col">
-            <label className="form-label" htmlFor="UKtimeinput">UK Time</label>
-            <input type="time" className="form-control" id="UKtimeinput" onChange={(e) => UKtoSG(e.target.value)} />
-          </div>
-          <div className="col">
-            <label className="form-label" htmlFor="SGtimeinput">SG Time</label>
-            <input type="time" className="form-control" id="SGtimeinput" onChange={(e) => SGtoUK(e.target.value)} />
-          </div>
-        </form>
-
-        <div className="row my-5 text-center">
-          <div className="col">
-            <h6>The time in UK would be:</h6>
-            <h4>{UKtime}</h4>
-          </div>
-          <div className="col">
-            <h6>The time in SG would be:</h6>
-            <h4>{SGtime}</h4>
-          </div>
+      <div className="row my-5 text-center">
+        <div className="col-auto mx-auto">
+          <h4>London</h4>
+          <h5>{getTimeZone("Europe/London")}</h5>
+        </div>
+        <div className="col-auto mx-auto">
+          <h4>Singapore</h4>
+          <h5>{getTimeZone("Asia/Singapore")}</h5>
         </div>
       </div>
     </div>
+
+    <div className="Container">
+      <form className="row my-3 text-center">
+        <div className="col-auto mx-auto">
+          <label className="form-label" htmlFor="UKtimeinput">UK Time</label>
+          <input type="time" className="form-control" id="UKtimeinput" onChange={(e) => UKtoSG(e.target.value)} />
+        </div>
+        <div className="col">
+          <label className="form-label" htmlFor="SGtimeinput">SG Time</label>
+          <input type="time" className="form-control" id="SGtimeinput" onChange={(e) => SGtoUK(e.target.value)} />
+        </div>
+      </form>
+    </div>
+
+    <div className="container">
+      <div className="row my-5 text-center">
+        <div className="col-auto mx-auto">
+          <h4>The time in UK would be:</h4>
+          <h5>{UKtime}</h5>
+        </div>
+        <div className="col-auto mx-auto">
+          <h4>The time in SG would be:</h4>
+          <h5>{SGtime}</h5>
+        </div>
+      </div>
+    </div>
+
+
+  </>
   );
 }
 
