8dc7933171824a04db164445bc18893dbbec8137
zortol <nepal_14221@hotmail.com>
2021-09-02 11:28:15 +0100

    Fixed modulo error

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 
 function App() {
   const [now, setNow] = useState(new Date())
-  const [SGtime, setSGTime] = useState()
-  const [UKtime, setUKTime] = useState()
+  const [SGtime, setSGTime] = useState("--:--")
+  const [UKtime, setUKTime] = useState("--:--")
   const [Deltatime, setDeltaTime] = useState(7)
 
   const [CurrentUKTime, setCurrentUKTime] = useState()
@@ -19,10 +19,7 @@ function App() {
     else {
       return hr
     }
-
   }
-
-
 
   const UKtoSG = (time) => {  // changes UK time to SG 
     const hr = parseInt(time.slice(0, -3))
@@ -30,6 +27,7 @@ function App() {
     const min = (time.slice(3, 6))
     const stringTime = hrConverted + ":" + min
     setSGTime(stringTime)
+    setUKTime("--:--")
   }
   const SGtoUK = (time) => {  // changes SG time to UK
     const hr = parseInt(time.slice(0, -3))
@@ -37,69 +35,66 @@ function App() {
     const min = (time.slice(3, 6))
     const stringTime = hrConverted + ":" + min
     setUKTime(stringTime)
+    setSGTime("--:--")
   }
 
   const getTimeZone = (timezone) => {
     let date = now.toLocaleString("en-GB", { timeZone: timezone, timeStyle: "short", });
-    console.log('Date in ' + timezone + ': ' + date);
-    return date
-  }
-
-  const getTimeZoneName = (timezone) => {
-    let date = now.toLocaleString("en-GB", { timeZoneName: "long", timeZone: timezone });
+    console.log('Time in ' + timezone + ': ' + date);
     return date
   }
 
 
-  useEffect(() => {
-    getTimeZone("Europe/London")
-    getTimeZone("Asia/Singapore")
-
-  }, [0])
-
-  return (<>
-
-    <div className="container">
-      <div className="row my-5 text-center">
-        <div className="col-auto mx-auto">
-          <h4>London</h4>
-          <h5>{getTimeZone("Europe/London")}</h5>
+  return (
+    <>
+      <div className="container app-container my-5">
+        <div className="rounded-top p-4">
+          <div className="row text-center">
+            <div className="col-auto mx-auto">
+              <h4>London</h4>
+              <h5>{getTimeZone("Europe/London")}</h5>
+            </div>
+            <div className="col-auto mx-auto">
+              <h4>Singapore</h4>
+              <h5>{getTimeZone("Asia/Singapore")}</h5>
+            </div>
+          </div>
         </div>
-        <div className="col-auto mx-auto">
-          <h4>Singapore</h4>
-          <h5>{getTimeZone("Asia/Singapore")}</h5>
-        </div>
-      </div>
-    </div>
 
-    <div className="Container">
-      <form className="row my-3 text-center">
-        <div className="col-auto mx-auto">
-          <label className="form-label" htmlFor="UKtimeinput">UK Time</label>
-          <input type="time" className="form-control" id="UKtimeinput" onChange={(e) => UKtoSG(e.target.value)} />
+        <div className="p-4">
+          <form className="row text-center d-flex justify-content-center">
+            <div className="col-4 mx-auto">
+              <label className="form-label" htmlFor="UKtimeinput">UK Time</label>
+              <input type="time" className="form-control" id="UKtimeinput" onChange={(e) => UKtoSG(e.target.value)} />
+            </div>
+            <div className="col-4 mx-auto">
+              <label className="form-label" htmlFor="SGtimeinput">SG Time</label>
+              <input type="time" className="form-control" id="SGtimeinput" onChange={(e) => SGtoUK(e.target.value)} />
+            </div>
+          </form>
         </div>
-        <div className="col">
-          <label className="form-label" htmlFor="SGtimeinput">SG Time</label>
-          <input type="time" className="form-control" id="SGtimeinput" onChange={(e) => SGtoUK(e.target.value)} />
-        </div>
-      </form>
-    </div>
 
-    <div className="container">
-      <div className="row my-5 text-center">
-        <div className="col-auto mx-auto">
-          <h4>The time in UK would be:</h4>
-          <h5>{UKtime}</h5>
-        </div>
-        <div className="col-auto mx-auto">
-          <h4>The time in SG would be:</h4>
-          <h5>{SGtime}</h5>
+        <div className="rounded-bottom rounded-3 p-4">
+          <div className="row text-center">
+            <div className="col-auto mx-auto">
+              <h5>The time in UK would be:</h5>
+              <h5>{UKtime}</h5>
+            </div>
+            <div className="col-auto mx-auto">
+              <h5>The time in SG would be:</h5>
+              <h5>{SGtime}</h5>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
 
+      <div class="container-fluid py-2 fixed-bottom">
+        <footer class="footer text-center">
+          <span class="text-muted"><small>All rights reserved. Siddharth Gurung and Daniel Olaszy, 2021</small></span>
+        </footer>
+      </div>
 
-  </>
+    </>
   );
 }
 
